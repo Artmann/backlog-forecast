60952a33740c3332a168c3e22ab70d5760a64f4e
Christoffer Artmann <artgaard@gmail.com>
2018-11-10 00:35:40 +0100

    Initial commit

---
+++ src/app/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        Hello
+      </div>
+    );
+  }
+}
+
+export default App;
